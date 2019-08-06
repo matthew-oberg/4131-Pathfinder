ecb0c68f4a90599f7321ce459083733f849a5abe
Matthew Oberg <matthewroberg@gmail.com>
2019-08-06 11:33:07 -0700

    Turn and Drive PID Controllers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import com.kauailabs.navx.frc.*;
 
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -19,6 +20,24 @@ import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
 
+  private static final double pTurn = 0.0;
+  private static final double iTurn = 0.0;
+  private static final double dTurn = 0.0;
+  private static final double fTurn = 0.0;
+  
+  private static final double toleranceDegrees = 2.0;
+  
+  private static int turnTarget = 0;
+  
+  private static final double pDrive = 0.0;
+  private static final double iDrive = 0.0;
+  private static final double dDrive = 0.0;
+  private static final double fDrive = 0.0;
+  
+  private static final double toleranceTicks = 25.0;
+  
+  private static int driveTarget = 0;
+  
   TalonSRX leftOne = new TalonSRX(1);
   TalonSRX leftTwo = new TalonSRX(2);
   TalonSRX rightOne = new TalonSRX(3);
@@ -29,10 +48,25 @@ public class Robot extends TimedRobot {
   GenericHID.Hand rightHand = GenericHID.Hand.kRight;
 
   AHRS ahrs = new AHRS(SPI.Port.kMXP);
+  
+  PIDController turn = new PIDController(pTurn, iTurn, dTurn, fTurn, ahrs, this);
+  PIDController drive = new PIDController(pDrive, iDrive, dDrive, dDrive, leftOne.getSelectedSensorPosition(), this);
 
   @Override
   public void robotInit() {
-
+    turn.setInputRange(-180.0, 180.0);
+    turn.setOutputRange(-1, 1);
+    turn.setAbsoluteTolerance(toleranceDegrees);
+    turn.setContinuous(true);
+    turn.enable();
+    turn.setSetpoint(0);
+    
+    drive.setInputRange(-254000.0, 254000.0);
+    drive.setOutputRange(-1, 1);
+    drive.setAbsoluteTolerance(toleranceTicks);
+    drive.setContinuous(true);
+    drive.enable();
+    drive.setSetpoint(0);
   }
 
   @Override
@@ -56,7 +90,6 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     standardDrive();
-    System.out.println(ahrs.getAngle() * 4.5);
   }
 
   public void standardDrive() {
@@ -74,5 +107,9 @@ public class Robot extends TimedRobot {
 
     rightOne.set(ControlMode.PercentOutput, turn + throttle);
     rightTwo.set(ControlMode.PercentOutput, turn + throttle);
+  }
+  
+  public void reset() {
+    ahrs.reset();
   }
 }
